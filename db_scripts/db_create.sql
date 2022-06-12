07254a4d89c5bb640068aa5ef0f031c9283465ca
Kumar <KUMNITI@HILTI.com>
2022-06-12 17:43:00 +0530

    added db creation script

---
+++ db_scripts/db_create.sql
@@ -0,0 +1,8 @@
+CREATE DATABASE online_judge
+    WITH
+    OWNER = postgres
+    ENCODING = 'UTF8'
+    CONNECTION LIMIT = -1;
+
+COMMENT ON DATABASE online_judge
+    IS 'Database for an online judge.';
