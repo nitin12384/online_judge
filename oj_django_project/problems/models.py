04c0819053334c9c16e598a0b209bf3dd03bd0bc
nitin12384 <nknitinkumar12384@gmail.com>
2022-06-19 19:19:57 +0530

    add models

--- oj_django_project/problems/models.py
+++ oj_django_project/problems/models.py
@@ -1,3 +1,57 @@
 from django.db import models
 
 # Create your models here.
+
+"""
+Fields 
+id (default) ,
+name,
+data_dir_path,
+difficulty,
+num_testcases,
+time_limit
+"""
+class Problem(models.Model) :
+    name = models.CharField(max_length=200)
+    data_dir_path = models.CharField(max_length=200)
+    difficulty = models.IntegerField()
+    num_testcases = models.IntegerField()
+    time_limit = models.FloatField()
+
+"""
+id : int, primary key
+problem_id : int foreign key
+source_file_path : string
+verdict : string 
+runtime : int, (in msec)
+language_id : int 
+submission_time : timestamp
+
+"""
+class Submission(models.Model) :
+    problem_id = models.ForeignKey(Problem, on_delete=models.CASCADE)
+    source_file_path = models.CharField(max_length=200)
+    verdict = models.CharField(max_length=20)
+    runtime = models.IntegerField()
+    language_id = models.IntegerField()
+    submission_time = models.DateTimeField()
+
+"""
+id,
+name
+"""
+
+class Language(models.Model) :
+    name = models.CharField(30)
+
+"""
+problem_id,
+language_id
+"""
+
+class ProblemLanuageRelation(models.Model) :
+    problem_id = models.ForeignKey(Problem, on_delete=models.CASCADE)
+    language_id = models.IntegerField()
+    
+
+    
