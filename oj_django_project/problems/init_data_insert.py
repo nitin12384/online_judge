ae3e6b195937e0e09d0a60b8a610533a32fc37c0
nitin12384 <nknitinkumar12384@gmail.com>
2022-06-19 22:25:20 +0530

    add second problem data

--- oj_django_project/problems/init_data_insert.py
+++ oj_django_project/problems/init_data_insert.py
@@ -4,18 +4,34 @@
 from problems.models import Problem,Submission,Language,ProblemLanguageRelation
 from django.utils import timezone
 
-new_problem =   Problem(name='Max Matrix Row', data_dir_path='/1', difficulty=600, 
-            num_testcases=2, time_limit=1.0)
+
+
+new_problem =   Problem(name='Max Matrix Row', data_dir_path='/1', difficulty=600, num_testcases=2, time_limit=1.0)
 new_problem.save()
 assert new_problem.id == 1 
+
+new_problem =   Problem(name='Amazon - Robot With Strings', data_dir_path='/2', difficulty=1300, num_testcases=2, time_limit=1.0)
+new_problem.save()
+assert new_problem.id == 2 
+
+
 
 new_language = Language(name='C++ 14')
 new_language.save()
 assert new_language.id == 1
 
+
+
+
 new_plr = ProblemLanguageRelation(problem=Problem.objects.get(pk=1), language_id=1)
 new_plr.save()
 assert new_plr.id == 1
+
+new_plr = ProblemLanguageRelation(problem=Problem.objects.get(pk=2), language_id=1)
+new_plr.save()
+assert new_plr.id == 2
+
+
 
 new_submission = Submission(problem = Problem.objects.get(pk=1), source_file_path = 'data1/1.cpp', verdict = 'WA on Test Case 1', runtime = 1, language_id = 1, submission_time =  timezone.now())
 new_submission.save()
