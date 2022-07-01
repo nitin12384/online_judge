55c9deadfe86664b262e6c63e98a1c76e5cc0650
nitin12384 <nknitinkumar12384@gmail.com>
2022-07-01 15:50:03 +0530

    add processing code

--- oj_django_project/problems/backend/CodeProcessor.py
+++ oj_django_project/problems/backend/CodeProcessor.py
@@ -1,8 +1,17 @@
 
 from .DatabaseHandler import get_problem
+from . import configs
+
+from ..models import Problem
 
 class LanguageProcessorBase:
-    pass
+    @staticmethod
+    def preprocess(code_file_full_path: str):
+        pass
+
+    @staticmethod
+    def check_security(code_file_full_path: str) -> bool:
+        return True
 
 
 class CPPLanguageProcessor(LanguageProcessorBase):
@@ -21,22 +30,35 @@ LanguageProcessorMapping = {
 def get_language_processor(language_id: int) -> LanguageProcessorBase:
     return LanguageProcessorMapping[language_id]
 
+def get_testcases_dir_path(problem: Problem) -> str:
+    return configs.PROBLEM_DATA_BASE_PATH + \
+            problem.data_dir_path + configs.TESTCASES_DIR
+
+
+def get_output_dir_path() -> str:
+    configs.SUBMISSION_DATA_BASE_PATH
+
 
 def process(code_file_full_path: str, language_id: int,
             submission_id: int, problem_id: int) -> str:
     # return verdict
     language_processor = get_language_processor(language_id)
     # Task : preprocess
+    language_processor.preprocess(code_file_full_path)
 
     # Task : Initial security checks etc.
+    if not language_processor.check_security(code_file_full_path):
+        return configs.SECURITY_CHECK_FAILED_VERDICT
 
     # Task : get input path, output path ..
+    problem = get_problem(problem_id)
+    num_testcases = problem.num_testcases
+    testcases_dir_path = get_testcases_dir_path(problem)
 
     # Task : compile and run and generate output
     # match output of testcase i, before running for testcase i+1
     # and get the verdict
 
-    problem = get_problem(problem_id)
 
     #
 
--- oj_django_project/problems/backend/configs.py
+++ oj_django_project/problems/backend/configs.py
@@ -3,8 +3,10 @@ NUM_SUBMISSION_IN_DIR = 100000
 DATA_HOME_WINDOWS = r"C:\Users\kumniti\prog\Projects\online_judge\data"
 SUBMISSION_DATA_BASE_PATH = DATA_HOME_WINDOWS + r"\submissions"
 PROBLEM_DATA_BASE_PATH = DATA_HOME_WINDOWS + r"\problems"
+TESTCASES_DIR = r"\testcases"
 
 ENVIRONMENT = "WINDOWS_1"
 ENV_WINDOWS1 = "WINDOWS_1"
 
 INITIAL_VERDICT_TEXT = "Processing"
+SECURITY_CHECK_FAILED_VERDICT = "Security check failed for your code"
