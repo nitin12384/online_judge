3261b32f4e832c2e402b31608b5e5fb21fac7c68
nitin12384 <nknitinkumar12384@gmail.com>
2022-07-24 13:05:26 +0530

    move cpp code to base lang processor

--- oj_django_project/problems/backend/code_processor.py
+++ oj_django_project/problems/backend/code_processor.py
@@ -8,6 +8,7 @@ from .execution import run_command, ExecutionInfo, ExecutionCap
 from ..models import Problem
 from .validation import file_comparer_exact
 
+from overrides import overrides
 
 # can use enum here
 class CompilationResult:
@@ -25,30 +26,10 @@ class LanguageProcessorBase:
     def check_security(self, code_file_full_path: str) -> bool:
         return True
 
-    # Override it
-    # Like abstract method
-    # returns the output file full
-    def create_executable_file(self, code_file_full_path: str,
-                               code_file_name_without_extension: str) -> tuple:
-        pass
 
+    
 
-class CPPLanguageProcessor(LanguageProcessorBase):
-    pass
-
-
-class CPP14LanguageProcessor(CPPLanguageProcessor):
-
-    def __init__(self):
-        # configs like compiler_path
-        self.compiler_full_path = configs.cur_config.cpp_compiler_path
-        self.executable_file_extension = ".out"
-
-    def get_executable_path(self, executable_dir_path: str, code_file_name_without_extension: str) -> str:
-        # dir path should not end with a slash ?
-
-        return executable_dir_path + configs.SLASH + code_file_name_without_extension \
-               + self.executable_file_extension
+    
 
     # may generate compiler error
     # returns : executable_file_path, compilation_result
@@ -67,22 +48,100 @@ class CPP14LanguageProcessor(CPPLanguageProcessor):
 
         return executable_file_path, compilation_result
 
+    def execute_file_with_io(self, executable_file_path: str, inp_file_path: str,
+                             out_file_path: str, runtime_limit: int, memory_limit: int) -> ExecutionInfo:
+        command = self.get_execute_command(executable_file_path, inp_file_path, out_file_path)
+        return run_command(command, ExecutionCap(runtime_limit, memory_limit))
+
+    def process(self, code_file_path: str, code_file_name_without_extension: str,
+                num_testcase: int, testcases_dir_path: str, output_dir_path: str,
+                runtime_limit: int, memory_limit: int) -> tuple:
+        # preprocess
+        self.preprocess(code_file_path)
+
+        # security check
+        if not self.check_security(code_file_path):
+            return configs.SECURITY_CHECK_FAILED_VERDICT
+
+        executable_file_path, compilation_result = self.create_executable_file(code_file_path,
+                                                                               code_file_name_without_extension)
+        # verdict_type : 0 -> AC, 1 -> WA, 2 -> RE, 3 -> TLE, 4 -> MLE, 5 -> CE
+        if compilation_result.failed :
+            # runtime 0
+            # verdict_type  5
+            return "Compilation Error", 5, 0
+
+        verdict = "Accepted : Passed #" + str(num_testcase) + " Test Cases."
+        verdict_type = 0
+        runtime = -1
+
+        for testcase_id in range(1, num_testcase + 1):
+            # get inp file path
+            inp_file_path = get_inp_file_path(testcases_dir_path, testcase_id)
+            expected_output_file_path = get_expected_output_file_path(testcases_dir_path, testcase_id)
+
+            # get out file path
+            out_file_path = get_output_file_path(output_dir_path, testcase_id)
+
+            # execute
+            execution_info = self.execute_file_with_io(executable_file_path, inp_file_path, out_file_path,
+                                                       runtime_limit, memory_limit)
+
+            runtime = max(runtime, execution_info.runtime)
+
+            if execution_info.runtime_cap_reached :
+                verdict = "TLE on TestCase #" + str(testcase_id)
+                verdict_type = 3
+                break
+
+            if execution_info.failed:
+                verdict = execution_info.message + " on TestCase #" + str(testcase_id)
+                verdict_type = 2
+                break
+
+            # compare and update verdict
+            if not file_comparer_exact(out_file_path, expected_output_file_path):
+                verdict = "Wrong Answer on TestCase #" + str(testcase_id)
+                verdict_type = 1
+                break
+
+        return verdict, verdict_type, runtime
+
+
+
+
+class CPPLanguageProcessor(LanguageProcessorBase):
+    pass
+
+
+# Todo
+class PythonLangaugeProcessor(LanguageProcessorBase):
+    pass
+
+class CPP14LanguageProcessor(CPPLanguageProcessor):
+
+    def __init__(self):
+        # configs like compiler_path
+        self.compiler_full_path = configs.cur_config.cpp_compiler_path
+        self.executable_file_extension = ".out"
+
+
     def get_compiler_command(self, code_file_path: str, executable_file_full_path: str) -> str:
         return self.compiler_full_path + configs.SPACE + \
                quote_enclose(code_file_path) + " -o " + quote_enclose(executable_file_full_path)
 
-    @staticmethod
+    def get_executable_path(self, executable_dir_path: str, code_file_name_without_extension: str) -> str:
+        # dir path should not end with a slash ?
+
+        return executable_dir_path + configs.SLASH + code_file_name_without_extension \
+               + self.executable_file_extension
+
     def get_execute_command(executable_file_path: str, inp_file_path: str,
                             out_file_path: str):
         # for c++ executables
         return "type " + quote_enclose(inp_file_path) + \
             " | " + quote_enclose(executable_file_path) + \
             " > " + out_file_path
-
-    def execute_file_with_io(self, executable_file_path: str, inp_file_path: str,
-                             out_file_path: str, runtime_limit: int, memory_limit: int) -> ExecutionInfo:
-        command = self.get_execute_command(executable_file_path, inp_file_path, out_file_path)
-        return run_command(command, ExecutionCap(runtime_limit, memory_limit))
 
     def process(self, code_file_path: str, code_file_name_without_extension: str,
                 num_testcase: int, testcases_dir_path: str, output_dir_path: str,
