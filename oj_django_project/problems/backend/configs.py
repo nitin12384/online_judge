aa4731bf3a242405c011af66db335ae221a8303a
nitin12384 <nknitinkumar12384@gmail.com>
2022-07-24 12:35:52 +0530

    add more cofig

--- oj_django_project/problems/backend/configs.py
+++ oj_django_project/problems/backend/configs.py
@@ -47,6 +47,12 @@ class Config:
         self.cpp_compiler_path    = cpp_compiler_path   
         self.python_compiler_path = python_compiler_path
 
+        self.submission_data_dir_path = self.data_home + SUBMISSION_DATA_RELATIVE_PATH
+        self.problem_data_dir_path = self.data_home + PROBLEM_DATA_RELATIVE_PATH
+        self.temp_out_data_dir_path = self.submission_data_dir_path + TEMP_OUTPUT_DATA_RELATIVE_PATH
+        self.temp_executable_dir_path = self.submission_data_dir_path + TEMP_EXECUTABLE_DIR_RELATIVE_PATH
+
+
 ubuntu_1_config = Config(ENV_UBUNTU1, DATA_HOME_UBUNTU_1, CPP_COMPILER_PATH_2, PYTHON_COMPILER_PATH_2)
 window_1_config = Config(ENV_WINDOWS1, DATA_HOME_WINDOWS_1, CPP_COMPILER_PATH_1, PYTHON_COMPILER_PATH_1)
 
@@ -56,4 +62,3 @@ if ENVIRONMENT == ENV_UBUNTU1 :
     cur_config = ubuntu_1_config
 elif ENVIRONMENT == ENV_WINDOWS1 :
     cur_config = window_1_config
-    
