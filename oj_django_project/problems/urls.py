e496f003d5d35973646312a47349831f56871dc0
nitin12384 <nknitinkumar12384@gmail.com>
2022-07-26 15:09:07 +0530

    add auth urls

--- oj_django_project/problems/urls.py
+++ oj_django_project/problems/urls.py
@@ -10,5 +10,14 @@ urlpatterns = [
     
     path('profile/<str:username>', views.user_profile, name='user_profile_path'),
     
-    path('submit/', views.submit, name='submit_path') # for the post request
+    path('submit/', views.submit, name='submit_path'), # for the post request
+
+    ## Auth urls
+    path('login_page/', views.login_page, name='login_page_path'),
+    path('signup_page/', views.signup_page, name='signup_page_path'),
+    
+    ## Auth actions urls for post request
+    path('login_action', views.login_action, name='login_action_path'),
+    path('signup_action', views.signup_action, name='signup_action_path'),
+    path('logout_action', views.logout_action, name='logout_action_path'),
 ]
