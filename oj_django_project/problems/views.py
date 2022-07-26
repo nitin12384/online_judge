9b098b406ba13b496060a43724bcf2dd71e69773
nitin12384 <nknitinkumar12384@gmail.com>
2022-07-26 13:51:29 +0530

    add 404 and add code to get user context

--- oj_django_project/problems/views.py
+++ oj_django_project/problems/views.py
@@ -2,12 +2,15 @@ from cmath import log
 import json
 from django.http import HttpResponse, JsonResponse
 from django.shortcuts import render, get_object_or_404
+
+from .backend import configs
 from .models import Problem, Submission
 from .backend.utils import get_problem_detailed_context
 from django.views.decorators.csrf import csrf_exempt
 from .backend.utils import Logger
-
 from .backend.core import SubmissionHandler
+
+from django.contrib.auth.models import User
 # Create your views here.
 
 from .backend import db_handler
@@ -66,7 +69,71 @@ def submit(request):
 
     return JsonResponse(verdict_dict)
 
+# Todo : Move code to functions, modularize it
 # View for profile
 def user_profile(request, username):
-    context = {}
-    return render(request, 'problems/profile.html', context)
+    user = get_user_obj(username)
+    if user is None:
+        # 404 page
+        return render(request, 'problems/404.html')
+    else:
+        is_logged_in = is_user_logged_in(request, username)
+        user_private_info = None 
+        if is_logged_in :
+            user_private_info = get_user_private_info(user)
+
+        context = {
+            'user_public_info' : get_user_public_info(user),
+            'is_logged_in' : is_logged_in,
+            'user_private_info' : user_private_info
+        }
+        return render(request, 'problems/profile.html', context)
+
+
+## Utility Code : Later, move them to seperate file
+
+class UserPublicInfo:
+    def __init__(self,
+        username            ,
+        full_name           ,
+        num_problems_solved ,
+    ):
+        self.is_valid             = True            
+        self.username             = username            
+        self.full_name            = full_name           
+        self.num_problems_solved  = num_problems_solved 
+    def __init__(self):
+        self.is_valid = False
+
+class UserPrivateInfo:
+    def __init__(self,
+        email
+    ):
+        self.email = email
+
+def get_user_obj(username : str):
+    user_qset = User.objects.filter(username=username)
+
+    if user_qset.exists() :
+        return user_qset[0]
+    else:
+        return None
+
+def get_user_public_info(user_obj) -> UserPublicInfo:
+    username = user_obj.username
+    user_id = user_obj.id 
+    full_name = user_obj.first_name + configs.SPACE + user_obj.last_name
+    num_problems_solved = 0
+    return UserPublicInfo(username, full_name, num_problems_solved)
+
+def get_user_private_info(user_obj) -> UserPrivateInfo:
+    email = user_obj.email 
+    return UserPrivateInfo(email)
+
+
+def is_user_logged_in(request, username):
+    # Todo : Find out what user.is_active means
+    if request.user.is_anonymous :
+        return True
+    else :
+        return request.user.username == username 
