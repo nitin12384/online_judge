2a638e8fb744bc0550de5c6f569bae49f14282ae
nitin12384 <nknitinkumar12384@gmail.com>
2023-06-07 22:23:39 +0530

    finished Executor class

--- oj_django_project/problems/backend/execution.py
+++ oj_django_project/problems/backend/execution.py
@@ -4,23 +4,49 @@ from . import configs
 from .utils import Logger
 
 import os
+import psutil
+import threading
+import subprocess
+import time
+import datatime
 
 class ExecutionInfo:
-    def __init__(self, memory_usage: int, runtime: int, failed: bool = False,
+    def __init__(self, memory_usage: int = -1, runtime: int = -1, failed: bool = True,
                  return_code: int = 0, runtime_cap_reached: bool=False,
                  memory_cap_reached: bool=False,
                  message: str = None):
-        self.memory_usage = memory_usage
+        self.memory_usage = memory_usage # peak memory usage
         self.runtime = runtime
         self.failed = failed
         self.return_code = return_code
         self.runtime_cap_reached = runtime_cap_reached
         self.memory_cap_reached = memory_cap_reached
-        self.message = message
+        self.message = message # when code is failed, but time/memory limit are not exceeded
 
 
 class Executor:
-    def __init__(self, run_args, tlimit: float, memlimit: int):
+    def_memchecker_period = 0.1 # seconds
+    @staticmethod
+    def memcheck_worker(pid : int, memlim_bytes : int, check_delay_sec: float, result : ExecutionInfo):
+        proc_obj = psutil.Process(pid)
+        result.memory_cap_reached = False
+        result.memory_usage = 0
+
+        while proc_obj.is_running() :
+
+            mem_used = proc_obj.memory_info().rss
+            result.memory_usage = max(mem_used, result.memory_usage)
+            if mem_used > memlim_bytes :
+                # need to somehow also return the information to thread starter
+                result.memory_cap_reached = True
+                proc_obj.kill()
+            time.sleep(check_delay_sec)
+        
+        # convert to KB ?
+        result.memory_usage //= 1024
+
+    
+    def __init__(self, exec_args : list, tlimit: float, memlimit: int):
         # file paths
         self.stdin_file = None
         self.stdout_file = None
@@ -29,10 +55,68 @@ class Executor:
         self.tlimit = tlimit
         self.memlimit = memlimit
 
-        self.run_args = run_args
+        self.exec_args = exec_args
     
-    def execute() -> ExecutionInfo:
-        pass    
+    def init_io(self):
+        self.inp,self.out,self.error = None, None, None 
+        if self.stdin_file :
+            self.inp = open(self.stdin_file, 'r')
+        if self.stdout_file:
+            self.out = open(self.stdout_file, 'w')
+        if self.stderr_file:
+            self.error = open(self.stderr_file, 'w')
+    
+    def close_io(self):
+        if self.stdin_file:
+            self.inp.close()
+        if self.stdout_file:
+            self.out.flush()
+            self.out.close()
+        if self.stderr_file:
+            self.error.flush()
+            self.error.close()
+    
+    def execute(self) -> ExecutionInfo:
+
+        self.init_io()
+
+        proc = subprocess.Popen(self.exec_args, stdin=self.inp, stdout=self.out, stderr=self.error)
+        result = ExecutionInfo()
+
+        memchecker_thread = threading.Thread(target=Executor.memcheck_worker,
+        args=[proc.pid, self.memlimit, Executor.def_memchecker_period,result])
+        memchecker_thread.start()
+
+        t_start = datatime.datetime.now()
+
+        # Time Limit checker code
+        try:
+            proc.wait(timeout=self.tlimit)
+        except subprocess.TimeoutExpired:
+            proc.kill()
+            result.runtime_cap_reached = True
+        
+        # Join memchecker_thread
+        memchecker_thread.join()
+
+        t_end = datatime.datatime.now()
+        result.runtime = (t_end - t_start).total_seconds()*1000 # msecs
+
+        # Close IO Files
+        self.close_io()
+
+        # Finalize result
+        self.return_code = proc.poll()
+
+        if self.return_code != 0:
+            result.failed = True 
+            result.message = configs.RUNTIME_ERROR_MESSAGE
+
+        result.failed |= result.memory_cap_reached | result.runtime_cap_reached
+        
+        return result
+
+
 
 
 
